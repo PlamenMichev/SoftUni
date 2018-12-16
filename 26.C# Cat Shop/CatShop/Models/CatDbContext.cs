fd6d9651d2e1ef95acd4dd30d67c91c334d1886a
PlamenMichev <michev10@abv.bg>
2018-12-16 06:28:02 +0200

    Adding Exercises

---
+++ 26.C# Cat Shop/CatShop/Models/CatDbContext.cs
@@ -0,0 +1,15 @@
+﻿namespace CatShop.Models
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class CatDbContext : DbContext
+    {
+        public DbSet<Cat> Cats { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost\SQLEXPRESS;Database=Cats;Trusted_Connection=True;");
+        }
+    }
+}
