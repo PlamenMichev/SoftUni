4bd8a4dbfdb26c31509b8030354ff75eb84307c4
PlamenMichev <michev10@abv.bg>
2019-05-21 12:40:49 +0300

    Add exercise

--- 03.ObjectsAndDom-Lab/08. Visited Websites/solution.js
+++ 03.ObjectsAndDom-Lab/08. Visited Websites/solution.js
@@ -1,4 +1,13 @@
 function solve() {
-
-  //TODO...
+  let siteElements = Array.from(document.getElementsByClassName('link-1'));
+  for (let siteElement of siteElements) {
+    siteElement.addEventListener('click', (e) => {
+      let currentTarget = e.currentTarget;
+      let textElement = currentTarget.getElementsByTagName('p')[0];
+      let text = (textElement.textContent).split(' ');
+      text[1] = Number(text[1]) + 1;
+      textElement.textContent = text.join(' ');
+    });
+  }
+  
 }
