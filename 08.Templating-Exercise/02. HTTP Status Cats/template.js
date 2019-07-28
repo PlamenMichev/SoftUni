e41575c24449529e96d7a45222720699f4378f6e
PlamenMichev <michev10@abv.bg>
2019-07-28 14:22:54 +0300

    Add templating exercise

--- 08.Templating-Exercise/02. HTTP Status Cats/template.js
+++ 08.Templating-Exercise/02. HTTP Status Cats/template.js
@@ -1,8 +1,34 @@
 (() => {
-     renderCatTemplate();
+    const elements = {
+        sectionToShow: document.querySelector('#allCats ul'),
+    }
+    renderCatTemplate();
 
-     function renderCatTemplate() {
-         // TODO: Render cat template and attach events
-     }
- 
-})
+    async function renderCatTemplate() {
+        const handleClick = function () {
+            const currentElement = this.parentNode;
+            const detailsElement = currentElement.querySelector('.status');
+            detailsElement.style.display = detailsElement.style.display === 'none'
+                ? detailsElement.style.display = 'block'
+                : detailsElement.style.display = 'none';
+        }
+
+        const { cats } = window;
+        const template = document.getElementById('cat-template').innerHTML;
+        const templateFunc = Handlebars.compile(template);
+        const fragment = document.createDocumentFragment();
+        fragment.innerHTML = '';
+        for (const cat of cats) {
+            cat.imageLocation = `./images/${cat.imageLocation}.jpg`;
+            fragment.innerHTML += templateFunc(cat);
+        }
+
+        elements.sectionToShow.innerHTML = fragment.innerHTML;
+
+        const buttons = elements.sectionToShow.querySelectorAll('button');
+        for (const button of buttons) {
+            button.addEventListener('click', handleClick);
+        }
+    }
+
+})();
