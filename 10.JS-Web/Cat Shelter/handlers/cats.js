3c7ec09cfa217fb62c00468291f9c3bc3209ba8b
PlamenMichev <michev10@abv.bg>
2020-05-25 11:54:05 +0300

    Add crud functionallity in node server

--- 10.JS-Web/Cat Shelter/handlers/cats.js
+++ 10.JS-Web/Cat Shelter/handlers/cats.js
@@ -4,7 +4,7 @@ const path = require('path');
 const qs = require('querystring');
 const formidable = require('formidable');
 const breeds = require('../data/breeds.json');
-// const cats = require('../data/cats.json');
+const cats = require('../data/cats.json');
 
 module.exports = async (req, res) => {
     const pathname = url.parse(req.url).pathname;
@@ -22,15 +22,49 @@ module.exports = async (req, res) => {
                 return;
             }
 
+            let catBreedPlaceholder = breeds.map((breed) => `<option value=${breed}>${breed}</option>`);
+            let modifiedData = data.toString().replace('{{catBreeds}}', catBreedPlaceholder);
+
             res.writeHead(200, {
                 'Content-Type': 'text/html'
             });
 
-            res.write(data);
+            res.write(modifiedData);
             res.end();
         });
-    }
-    else if (pathname === '/cats/add-breed' && req.method === 'GET') {
+    } else if (pathname === '/cats/add-cat' && req.method === 'POST') {
+        let form = new formidable.IncomingForm();
+        form.uploadDir = `D:\\Softuni-Courses(Repo)\\SoftUni\\10.JS-Web\\Cat Shelter\\content\\images`
+
+        form.parse(req, async (err, fields, files) => {
+            if (err) {
+                return err;
+            }
+
+            let picture = files;
+
+            let newCat = {
+                id: cats[cats.length - 1] == undefined ? 1 : cats[cats.length - 1].id + 1,
+                name: fields['name'],
+                description: fields['description'],
+                breed: fields['breed'],
+                image: picture.upload.path + `.${picture.upload.name.slice(picture.upload.name.indexOf('.') + 1)}`
+            };
+
+            cats.push(newCat);
+            console.log(cats);
+            await fs.writeFile(path.join(__dirname, '../data/cats.json'), JSON.stringify(cats), 'utf8', function(err) {});
+
+            await fs.readFile(path.join(__dirname, '../views/home/index.html'), (err, data) => {
+                res.writeHead(301, {
+                    'Location': '/'
+                });
+
+                res.write(data);
+                res.end();
+            });
+        });
+    } else if (pathname === '/cats/add-breed' && req.method === 'GET') {
         let filePath = path.join(`${__dirname}`, '../views/addBreed.html');
         console.log(filePath);
         await fs.readFile(filePath, (err, data) => {
@@ -51,8 +85,7 @@ module.exports = async (req, res) => {
             res.write(data);
             res.end();
         });
-    } 
-    else if (pathname === '/cats/add-breed' && req.method === 'POST') {
+    } else if (pathname === '/cats/add-breed' && req.method === 'POST') {
         let body = '';
 
         req.on('data', function (data) {
